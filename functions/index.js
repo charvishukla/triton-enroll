55336b3ce159182bb36dd876c050a45fb6d12a2f
Charvi Shukla <67646308+charvishukla@users.noreply.github.com>
2023-06-23 04:21:52 -0500

    changes

--- functions/index.js
+++
@@ -1,9 +0,0 @@
-const functions = require("firebase-functions");
-
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-exports.sumNumbers = functions.https.onCall((data, context) => {
-  functions.logger.log(`${data.firstNumber} + ${data.secondNumber} = ${data.firstNumber + data.secondNumber}`);
-  return (data.firstNumber + data.secondNumber);
-});
